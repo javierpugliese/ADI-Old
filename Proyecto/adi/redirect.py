084b3febccecaa1546470ba12efcd7bdfa75abca
MirandaGonzalo <gonzamirandab2000@gmail.com>
2017-10-05 11:15:03 -0300

    Se agrega una Modal que da una breve descripcion del proyecto, la cual se abre cuando se hace clic en el logo del proyecto en el menú de inicio

---
+++ Proyecto/adi/redirect.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render_to_response, render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from .models import *
+
+def preceptor(request):
+    return render(request, 'preceptor/index.html')
+
+def cpreceptor(request):
+    return render(request, 'admin/crear_preceptor.html')
+
+def index(request):
+    return render (request, "inicio.html")
+    #return render (request, "preceptor/charts.html")
+
+def index_guardia(request):
+    pepe = request.user
+    if pepe.email.endswith('@preceptor.com'):
+        return render(request, 'rechazado.html')
+    return render(request, 'guardia/index.html')
+
+def chalumno(request):
+    return render(request, 'admin/modificar_alumno.html')
+
+def calumno(request):
+    return render(request, 'admin/cargar_alumno.html')
+
+def guardia(request):
+    return render(request, 'guardia.html')
+
+def inicio(request):
+  return render(request, 'index.html')
+
+def ver_charts(request):
+    return render(request, 'preceptor/charts.html')
+
+def login_p(request):
+    return render(request, 'login.html')
--- Proyecto/adi/views.py
+++ Proyecto/adi/views.py
@@ -18,6 +18,7 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from .models import *
+from .redirect import *
 
 #Función que crea F2
 def crear_fm(request, dni_alumno):
@@ -238,44 +239,6 @@ def datos_formulario_guardia(request, id_for):
     return render(request,
                   'guardia/datos.html',
                   {'form':formi})
-
-
-# =========================================================================
-# Funciones de carga de template
-
-def preceptor(request):
-    return render(request, 'preceptor/index.html')
-
-def cpreceptor(request):
-    return render(request, 'admin/crear_preceptor.html')
-
-def index(request):
-    return render (request, "inicio.html")
-    #return render (request, "preceptor/charts.html")
-
-def index_guardia(request):
-    pepe = request.user
-    if pepe.email.endswith('@preceptor.com'):
-        return render(request, 'rechazado.html')
-    return render(request, 'guardia/index.html')
-
-def chalumno(request):
-    return render(request, 'admin/modificar_alumno.html')
-
-def calumno(request):
-    return render(request, 'admin/cargar_alumno.html')
-
-def guardia(request):
-    return render(request, 'guardia.html')
-
-def inicio(request):
-  return render(request, 'index.html')
-
-def ver_charts(request):
-    return render(request, 'preceptor/charts.html')
-
-def login_p(request):
-    return render(request, 'login.html')
 
 def f2(request):
     pepe = request.user
